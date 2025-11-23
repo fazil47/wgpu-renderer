a0fb976e1bf6d809151d17f056e3c5072952def3
Fazil Babu <fazil47babu@gmail.com>
2025-11-23 23:14:46 +0530

    chore: Upgrade wesl to 0.2

--- app/build.rs
+++ app/build.rs
@@ -1,12 +1,34 @@
+use std::str::FromStr;
+
+use wesl::ModulePath;
+
 fn main() {
     let is_wasm = std::env::var("TARGET").unwrap_or_default().contains("wasm");
 
     let mut resolver = wesl::Wesl::new("src/shaders");
     resolver.set_feature("wasm", is_wasm);
-    resolver.build_artefact("rasterizer/main.wesl", "rasterizer-main");
-    resolver.build_artefact("raytracer/render.wesl", "raytracer-render");
-    resolver.build_artefact("raytracer/compute.wesl", "raytracer-compute");
-    resolver.build_artefact("raytracer/bvh-lines.wesl", "raytracer-bvh-lines");
-    resolver.build_artefact("probe_lighting/updater.wesl", "probe-updater");
-    resolver.build_artefact("probe_lighting/visualization.wesl", "probe-visualization");
+    resolver.build_artifact(
+        &ModulePath::from_str("package::rasterizer::main").unwrap(),
+        "rasterizer-main",
+    );
+    resolver.build_artifact(
+        &ModulePath::from_str("package::raytracer::render").unwrap(),
+        "raytracer-render",
+    );
+    resolver.build_artifact(
+        &ModulePath::from_str("package::raytracer::compute").unwrap(),
+        "raytracer-compute",
+    );
+    resolver.build_artifact(
+        &ModulePath::from_str("package::raytracer::bvh_lines").unwrap(),
+        "raytracer-bvh-lines",
+    );
+    resolver.build_artifact(
+        &ModulePath::from_str("package::probe_lighting::updater").unwrap(),
+        "probe-updater",
+    );
+    resolver.build_artifact(
+        &ModulePath::from_str("package::probe_lighting::visualization").unwrap(),
+        "probe-visualization",
+    );
 }
