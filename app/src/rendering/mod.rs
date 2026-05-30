c0a0f9f1c55dda6ba4dbdf8c0776b5d4a2c35961
Fazil Babu <fazil47babu@gmail.com>
2026-05-30 14:27:49 +0530

    chore: tlas_bvh.rs -> bvh.rs

--- app/src/rendering/mod.rs
+++ app/src/rendering/mod.rs
@@ -1,14 +1,14 @@
+pub mod bvh;
 pub mod extract;
 pub mod mesh;
 pub mod rasterizer;
 pub mod raytracer;
 pub mod systems;
-pub mod tlas_bvh;
 mod vertex;
 pub mod wgpu;
 
+pub use bvh::{BlasBvh, TlasBvh, build_scene_bvh};
 pub use extract::{Extract, ExtractionError, WorldExtractExt};
 pub use rasterizer::Rasterizer;
 pub use raytracer::{Raytracer, RaytracerExtractedBuffers, RaytracerMaterial};
-pub use tlas_bvh::{BlasBvh, TlasBvh, build_scene_bvh};
 pub use vertex::GpuVertex;
